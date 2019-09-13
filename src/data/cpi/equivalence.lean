9057caed510d7d5ec118c196f55393a03425c945
SquidDev <bonzoweb@hotmail.co.uk>
2019-09-13 11:29:12 +0100

    Rethink how equivalence is implemented
    The general gist is to view them as a chain of rewrite rules - this
    means we don't need to nest rules, merely chain them together.

--- src/data/cpi/equivalence.lean
+++ src/data/cpi/equivalence.lean
@@ -37,11 +37,28 @@ end name
 namespace species
   /-- An equivalency relationship over species.
 
-      Ideally this should be a type, but it means we do not have
-      acess to sizeof, which makes it impossible to reasonably argue
-      about terms. -/
+      This can be viewed as a sequence of rewrite rules from one term to
+      another.
+
+      Rewrite rules are chained together using the equiv.chain construction,
+      which means this structure proves equivalency by default.
+
+      We do not have explicit refl or symm rules however. ξ rules provide
+      projections into structures, which allow one to implement refl, and
+      all non-symmetric rules end up having forwards and backwards versions -
+      while this does lead to some duplication, it seems easier to reason
+      about than having a generic "x_symm" rule.
+
+      Ideally this should be a type, but it means we do not have access to
+      sizeof, which makes it more inconvenient to reason about the termination
+      of some programs.
+  -/
   inductive equiv : ∀ {Γ : context} (A B : species Γ), Type
-  -- ξ rules just map the insides
+  -- Chain two rewrite rules together.
+  | chain : ∀ {Γ} {A B C : species Γ}
+          , equiv A B → equiv B C → equiv A C
+
+  -- ξ rules act as projections into the body of a species.
   | ξ_nil : ∀ {Γ}, @equiv Γ nil nil
   | ξ_parallel :
       ∀ {Γ} {A A' B B' : species Γ}
@@ -58,55 +75,46 @@ namespace species
       → equiv (choice As) (choice As')
       → equiv (choice (choices.cons π A As)) (choice (choices.cons π A' As'))
 
-  -- And now we have our main equivalence rules. Note, in the non-symetric
-  -- cases, we have two rules, for A ≡ f(A) and f(A) ≡ A. While this is a
-  -- little ugly, the alternative "swap : equiv A B → equiv B A" rule, adds
-  -- even more complexities.
+  -- | An element in the choice array can be swapped.
+  | choice_swap :
+    ∀ {Γ} {f g} (π₁ : prefix_expr Γ f) (π₂ : prefix_expr Γ g)
+      {A : species (f Γ)} {B : species (g Γ)} {As : choices Γ}
+    , equiv (choice (choices.cons π₁ A (choices.cons π₂ B As)))
+            (choice (choices.cons π₂ B (choices.cons π₁ A As)))
 
-  | nil_parallel₁ :
-    ∀ {Γ} {A A' : species Γ}
-    , equiv A A'
-    → equiv (A |ₛ nil) A'
-  | nil_parallel₂ :
-    ∀ {Γ} {A A' : species Γ}
-    , equiv A A'
-    → equiv A' (A |ₛ nil)
+  -- Species forms a commutative semigroup over parallel (and nil).
+  | parallel_nil₁ : ∀ {Γ} {A : species Γ}, equiv (A |ₛ nil) A
+  | parallel_nil₂ : ∀ {Γ} {A : species Γ}, equiv A (A |ₛ nil)
+  | parallel_symm :   ∀ {Γ} {A B : species Γ}, equiv (A |ₛ B) (B |ₛ A)
+  | parallel_assoc₁ : ∀ {Γ} {A B C : species Γ}, equiv ((A |ₛ B) |ₛ C) (A |ₛ (B |ₛ C))
+  | parallel_assoc₂ : ∀ {Γ} {A B C : species Γ}, equiv (A |ₛ (B |ₛ C)) ((A |ₛ B) |ₛ C)
 
-  | parallel_symm :
-    ∀ {Γ} {A A' B B' : species Γ}
-    , equiv A A' → equiv B B'
-    → equiv (A |ₛ B) (B' |ₛ A')
-  | parallel_assoc₁ :
-    ∀ {Γ} {A A' B B' C C' : species Γ}
-    , equiv A A' → equiv B B' → equiv C C'
-    → equiv ((A |ₛ B) |ₛ C) (A' |ₛ (B' |ₛ C'))
-  | parallel_assoc₂ :
-    ∀ {Γ} {A A' B B' C C' : species Γ}
-    , equiv A A' → equiv B B' → equiv C C'
-    → equiv (A' |ₛ (B' |ₛ C')) ((A |ₛ B) |ₛ C)
-
+  -- Can move ν into a parallel branch when unused.
   | ν_parallel₁ :
-    ∀ {Γ} (M : affinity) {A B B': species (context.extend M.arity Γ)} {A' : species Γ}
-    , equiv A (subst name.extend A') → equiv B B'
-    → equiv (ν(M) (A |ₛ B)) (A' |ₛ ν(M)B')
+    ∀ {Γ} (M : affinity) {A : species Γ} {B : species (context.extend M.arity Γ)}
+    , equiv (ν(M) (subst name.extend A |ₛ B)) (A |ₛ ν(M)B)
   | ν_parallel₂ :
-    ∀ {Γ} (M : affinity) {A B B': species (context.extend M.arity Γ)} {A' : species Γ}
-    , equiv A (subst name.extend A') → equiv B B'
-    → equiv (A' |ₛ ν(M)B') (ν(M) (A |ₛ B))
+    ∀ {Γ} (M : affinity) {A : species Γ} {B : species (context.extend M.arity Γ)}
+    , equiv (A |ₛ ν(M)B) (ν(M) (subst name.extend A |ₛ B))
+
+  -- Can drop an unused ν
   | ν_drop₁ :
-    ∀ {Γ} (M : affinity) {A : species (context.extend M.arity Γ)} {A' : species Γ}
-    , equiv A (subst name.extend A')
-    → equiv (ν(M)A) A'
+    ∀ {Γ} (M : affinity) {A : species Γ}
+    , equiv (ν(M) subst name.extend A) A
   | ν_drop₂ :
-    ∀ {Γ} (M : affinity) {A : species (context.extend M.arity Γ)} {A' : species Γ}
-    , equiv A (subst name.extend A')
-    → equiv A' (ν(M)A)
-  | ν_swap :
+    ∀ {Γ} (M : affinity) {A : species Γ}
+    , equiv A (ν(M) subst name.extend A)
+
+  -- Can swap two ν variables safely. Ideally this one could be
+  -- a single rule, but the use of swap means it isn't.
+  | ν_swap₁ :
     ∀ {Γ} (M N : affinity)
-      {A  : species (context.extend N.arity (context.extend M.arity Γ))}
-      {A' : species (context.extend M.arity (context.extend N.arity Γ))}
-    , equiv A (subst name.swap A')
-    → @equiv Γ (ν(M)ν(N)A) (ν(N)ν(M)A')
+      {A : species (context.extend N.arity (context.extend M.arity Γ))}
+    , @equiv Γ (ν(M)ν(N)A) (ν(N)ν(M) subst name.swap A)
+  | ν_swap₂ :
+    ∀ {Γ} (M N : affinity)
+      {A : species (context.extend N.arity (context.extend M.arity Γ))}
+    , @equiv Γ (ν(N)ν(M) subst name.swap A) (ν(M)ν(N)A)
 
   /-- Lower equivalence to the Prop level. -/
   inductive is_equiv {Γ : context} (A B : species Γ) : Prop
@@ -127,47 +135,14 @@ namespace species
       dec_tac := tactic.fst_dec_tac,
     }
 
-    @[inline] private def depth : ∀ {Γ} {A B : species Γ}, A ~ B → ℕ
-    | ._ ._ ._ ξ_nil := 1
-    | ._ ._ ._ (ξ_parallel eq_a eq_b) := 1 + depth eq_a + depth eq_b
-    | ._ ._ ._ (ξ_restriction _ eq) := 1 + depth eq
-    | ._ ._ ._ ξ_choice_nil := 1
-    | ._ ._ ._ (ξ_choice_cons _ eq_a eq_as) := 1 + depth eq_a + depth eq_as
-
-    | ._ ._ ._ (nil_parallel₁ eq) := 1 + depth eq
-    | ._ ._ ._ (nil_parallel₂ eq) := 1 + depth eq
-
-    | ._ ._ ._ (parallel_symm eq_a eq_b) := 1 + depth eq_b + depth eq_a
-    | ._ ._ ._ (parallel_assoc₁ eq_a eq_b eq_c) := 1 + depth eq_a + depth eq_b + depth eq_c
-    | ._ ._ ._ (parallel_assoc₂ eq_a eq_b eq_c) := 1 + depth eq_a + depth eq_b + depth eq_c
-
-    | ._ ._ ._ (ν_parallel₁ M eq_a eq_b) := 1 + depth eq_a + depth eq_b
-    | ._ ._ ._ (ν_parallel₂ M eq_a eq_b) := 1 + depth eq_a + depth eq_b
-    | ._ ._ ._ (ν_drop₁ M eq_a) := 1 + depth eq_a
-    | ._ ._ ._ (ν_drop₂ M eq_a) := 1 + depth eq_a
-    | ._ ._ ._ (ν_swap M N eq) := 1 + depth eq
-
-    -- set_option profiler true
-
-    meta def depth_tactic : tactic unit := do
-      well_founded_tactics.unfold_wf_rel,
-      well_founded_tactics.unfold_sizeof,
-      tactic.dunfold_target [``depth],
-      tactic.trace_state,
-      tactic.try well_founded_tactics.cancel_nat_add_lt,
-      tactic.trace_state,
-      well_founded_tactics.trivial_nat_lt
-
     private lemma subst_ext
-      {Γ Δ} {ρ : name Γ → name Δ} {M : affinity}
-      (A : species (context.extend M.arity Γ)) (A' : species Γ)
+      {Γ Δ} {ρ : name Γ → name Δ} {M : affinity} (A' : species Γ)
       : subst name.extend (subst ρ A')
       = subst (name.ext ρ) (subst (@name.extend _ M.arity) A')
     := by rw [subst_compose, ← name.ext_extend, subst_compose]
 
     private lemma subst_swap
       {Γ Δ} {ρ : name Γ → name Δ} {M N : affinity}
-      (A : species (context.extend N.arity (context.extend M.arity Γ)))
       (A' : species (context.extend M.arity (context.extend N.arity Γ)))
       : subst (name.ext (name.ext ρ)) (subst name.swap A')
       = subst name.swap (subst (name.ext (name.ext ρ)) A')
@@ -178,8 +153,10 @@ namespace species
       ∀ {Γ Δ} {A B : species Γ} (ρ : name Γ → name Δ)
       , A ~ B
       → subst ρ A ~ subst ρ B
+    | Γ Δ ._ ._ ρ (@chain _ A B C ab bc) := chain (subst_pres ρ ab) (subst_pres ρ bc)
+
     | Γ Δ ._ ._ ρ ξ_nil := by { unfold subst, from ξ_nil }
-    | Γ Δ ._ ._ ρ (ξ_parallel eq_a eq_b) :=begin
+    | Γ Δ ._ ._ ρ (ξ_parallel eq_a eq_b) := begin
         unfold subst,
         from ξ_parallel (subst_pres ρ eq_a) (subst_pres ρ eq_b)
       end
@@ -197,181 +174,88 @@ namespace species
         from ξ_choice_cons _ (subst_pres (prefix_expr.ext π ρ) eq_a) as,
       end
 
-    | Γ Δ ._ ._ ρ (nil_parallel₁ eq) :=
-      by { unfold subst, from nil_parallel₁ (subst_pres ρ eq) }
-    | Γ Δ ._ ._ ρ (nil_parallel₂ eq) :=
-      by { unfold subst, from nil_parallel₂ (subst_pres ρ eq) }
-
-    | Γ Δ ._ ._ ρ (parallel_symm eq_a eq_b) := begin
-        unfold subst,
-        from parallel_symm (subst_pres ρ eq_a) (subst_pres ρ eq_b)
-      end
-    | Γ Δ ._ ._ ρ (parallel_assoc₁ eq_a eq_b eq_c) := begin
-        unfold subst,
-        from parallel_assoc₁ (subst_pres ρ eq_a) (subst_pres ρ eq_b) (subst_pres ρ eq_c)
-      end
-    | Γ Δ ._ ._ ρ (parallel_assoc₂ eq_a eq_b eq_c) := begin
-        unfold subst,
-        from parallel_assoc₂ (subst_pres ρ eq_a) (subst_pres ρ eq_b) (subst_pres ρ eq_c)
+    | Γ Δ ._ ._ ρ (@choice_swap _ f g π₁ π₂ A B As) := begin
+        simp only [subst, subst.choice],
+        from choice_swap (prefix_expr.subst ρ π₁) (prefix_expr.subst ρ π₂)
       end
 
-    | Γ Δ ._ ._ ρ (@ν_parallel₁ _ M A B B' A' eq_a eq_b) := begin
+    | Γ Δ ._ ._ ρ parallel_nil₁ := by { unfold subst, from parallel_nil₁ }
+    | Γ Δ ._ ._ ρ parallel_nil₂ := by { unfold subst, from parallel_nil₂ }
+    | Γ Δ ._ ._ ρ parallel_symm := by { unfold subst, from parallel_symm }
+    | Γ Δ ._ ._ ρ parallel_assoc₁ := by { unfold subst, from parallel_assoc₁ }
+    | Γ Δ ._ ._ ρ parallel_assoc₂ := by { unfold subst, from parallel_assoc₂ }
+
+    | Γ Δ ._ ._ ρ (@ν_parallel₁ _ M A B) := begin
         unfold subst,
-        have a : subst (name.ext ρ) A ~ subst name.extend (subst ρ A'),
-          rw subst_ext A A', from subst_pres (name.ext ρ) eq_a,
-        from ν_parallel₁ M a (subst_pres (name.ext ρ) eq_b)
+        rw ← subst_ext A,
+        from ν_parallel₁ M,
       end
-    | Γ Δ ._ ._ ρ (@ν_parallel₂ _ M A B B' A' eq_a eq_b) := begin
+    | Γ Δ ._ ._ ρ (@ν_parallel₂ _ M A B) := begin
         unfold subst,
-        have a : subst (name.ext ρ) A ~ subst name.extend (subst ρ A'),
-          rw subst_ext A A', from subst_pres (name.ext ρ) eq_a,
-        from ν_parallel₂ M a (subst_pres (name.ext ρ) eq_b)
+        rw ← subst_ext A,
+        from ν_parallel₂ M,
       end
-    | Γ Δ ._ ._ ρ (@ν_drop₁ _ M A A' eq) := begin
+    | Γ Δ ._ ._ ρ (@ν_drop₁ _ M A) := begin
         unfold subst,
-        have a : subst (name.ext ρ) A ~ subst name.extend (subst ρ A'),
-          rw subst_ext A A', from subst_pres (name.ext ρ) eq,
-        from ν_drop₁ M a
+        rw ← subst_ext A,
+        from ν_drop₁ M
       end
-    | Γ Δ ._ ._ ρ (@ν_drop₂ _ M A A' eq) := begin
+    | Γ Δ ._ ._ ρ (@ν_drop₂ _ M A) := begin
         unfold subst,
-        have a : subst (name.ext ρ) A ~ subst name.extend (subst ρ A'),
-          rw subst_ext A A', from subst_pres (name.ext ρ) eq,
-        from ν_drop₂ M a
+        rw ← subst_ext A,
+        from ν_drop₂ M
       end
-    | Γ Δ ._ ._ ρ (@ν_swap _ M N A A' eq) := begin
+    | Γ Δ ._ ._ ρ (@ν_swap₁ _ M N A) := begin
         unfold subst,
-        let a : subst (name.ext (name.ext ρ)) A ~ subst name.swap (subst (name.ext (name.ext ρ)) A'),
-          rw ← subst_swap A A', from subst_pres (name.ext (name.ext ρ)) eq,
-        from ν_swap M N a
+        rw subst_swap A,
+        from ν_swap₁ M N
+      end
+    | Γ Δ ._ ._ ρ (@ν_swap₂ _ M N A) := begin
+        unfold subst,
+        rw subst_swap A,
+        from ν_swap₂ M N
       end
 
-      /-- Equivalence is symmetric. -/
-      protected theorem symm : ∀ {Γ} {A B : species Γ}, A ~ B → B ~ A
-      | Γ ._ ._ ξ_nil := ξ_nil
-      | Γ ._ ._ (ξ_parallel eq_a eq_b) := ξ_parallel (symm eq_a) (symm eq_b)
-      | Γ ._ ._ (ξ_restriction M eq) := ξ_restriction M (symm eq)
-      | Γ ._ ._ ξ_choice_nil := ξ_choice_nil
-      | Γ ._ ._ (ξ_choice_cons π eq_a eq_as) := ξ_choice_cons π (symm eq_a) (symm eq_as)
+    /-- Equivalence is symmetric. -/
+    protected theorem symm : ∀ {Γ} {A B : species Γ}, A ~ B → B ~ A
+    | Γ ._ ._ (chain ab bc) := chain (symm bc) (symm ab)
 
-      | Γ ._ ._ (nil_parallel₁ eq) := nil_parallel₂ eq
-      | Γ ._ ._ (nil_parallel₂ eq) := nil_parallel₁ eq
+    | Γ ._ ._ ξ_nil := ξ_nil
+    | Γ ._ ._ (ξ_parallel eq_a eq_b) := ξ_parallel (symm eq_a) (symm eq_b)
+    | Γ ._ ._ (ξ_restriction M eq) := ξ_restriction M (symm eq)
+    | Γ ._ ._ ξ_choice_nil := ξ_choice_nil
+    | Γ ._ ._ (ξ_choice_cons π eq_a eq_as) := ξ_choice_cons π (symm eq_a) (symm eq_as)
 
-      | Γ ._ ._ (parallel_symm eq_a eq_b) := parallel_symm (symm eq_b) (symm eq_a)
-      | Γ ._ ._ (parallel_assoc₁ eq_a eq_b eq_c) := parallel_assoc₂ eq_a eq_b eq_c
-      | Γ ._ ._ (parallel_assoc₂ eq_a eq_b eq_c) := parallel_assoc₁ eq_a eq_b eq_c
+    | Γ ._ ._ (choice_swap π₁ π₂) := choice_swap π₂ π₁
 
-      | Γ ._ ._ (ν_parallel₁ M eq_a eq_b) := ν_parallel₂ M eq_a eq_b
-      | Γ ._ ._ (ν_parallel₂ M eq_a eq_b) := ν_parallel₁ M eq_a eq_b
-      | Γ ._ ._ (ν_drop₁ M eq_a) := ν_drop₂ M eq_a
-      | Γ ._ ._ (ν_drop₂ M eq_a) := ν_drop₁ M eq_a
-      | Γ ._ ._ (@ν_swap _ M N A A' eq) :=
-        let eql :=
-          calc  subst name.swap (subst name.swap A') ~ subst name.swap A
-              = subst (name.swap ∘ name.swap) A' ~ subst name.swap A
-                : by rw subst_compose name.swap name.swap A'
-          ... = subst id A' ~ subst name.swap A : by rw name.swap.twice_id
-          ... = A' ~ subst name.swap A : by rw subst_id
-        in begin
-          have a := equiv.subst_pres name.swap (symm eq),
-          rw eql at a,
-          from ν_swap N M a
-        end
+    | Γ ._ ._ parallel_nil₁ := parallel_nil₂
+    | Γ ._ ._ parallel_nil₂ := parallel_nil₁
+    | Γ ._ ._ parallel_symm := parallel_symm
+    | Γ ._ ._ parallel_assoc₁ := parallel_assoc₂
+    | Γ ._ ._ parallel_assoc₂ := parallel_assoc₁
 
-      protected theorem trans : ∀ {Γ} {A B C : species Γ}, A ~ B → B ~ C → A ~ C
-      | Γ ._ ._ _ ξ_nil eql := eql
-      | Γ ._ ._ _ ξ_choice_nil eql := eql
+    | Γ ._ ._ (ν_parallel₁ M) := ν_parallel₂ M
+    | Γ ._ ._ (ν_parallel₂ M) := ν_parallel₁ M
+    | Γ ._ ._ (ν_drop₁ M) := ν_drop₂ M
+    | Γ ._ ._ (ν_drop₂ M) := ν_drop₁ M
+    | Γ ._ ._ (ν_swap₁ M N) := ν_swap₂ M N
+    | Γ ._ ._ (ν_swap₂ M N) := ν_swap₁ M N
 
-      -- | Γ ._ ._ s (@ξ_parallel _ A A' B B' eq_a eq_b) eql := by
+    protected theorem trans : ∀ {Γ} {A B C : species Γ}, A ~ B → B ~ C → A ~ C := @chain
 
-      | Γ .(_ |ₛ _) .(_ |ₛ _) .(_ |ₛ _)
-        (@ξ_parallel _ A A' B B' eq_a eq_b)
-        (@ξ_parallel _ B1 B1' C C' eq_b1 eq_c) := by sorry
-      | Γ .(_ |ₛ _) .(_ |ₛ _) s
-        (@ξ_parallel _ A A' B B' eq_a eq_b)
-        eq := by sorry
+    section examples
+      variable Γ : context
+      variables A A' B C : species Γ
 
-      | Γ ._ ._ s (ξ_restriction M eq) eql := by sorry
-      | Γ ._ ._ s (ξ_choice_cons π eq_a eq_as) eql := by sorry
+      example : A ~ (A |ₛ nil) := parallel_nil₂
 
-      | Γ ._ ._ s (nil_parallel₁ eq) eql := by sorry
-      | Γ ._ ._ s (nil_parallel₂ eq) eql := by sorry
+      example : A ~ (nil |ₛ A) :=
+        chain parallel_nil₂ parallel_symm
 
-      | Γ ._ ._ s (parallel_symm eq_a eq_b) eql := by sorry
-      | Γ ._ ._ s (parallel_assoc₁ eq_a eq_b eq_c) eql := by sorry
-      | Γ ._ ._ s (parallel_assoc₂ eq_a eq_b eq_c) eql := by sorry
+      example : A ~ A' → (A |ₛ B) ~ C → (A' |ₛ B) ~ C := λ a eq,
+        equiv.trans (ξ_parallel (equiv.symm a) (equiv.refl B)) eq
 
-      | Γ ._ ._ s (ν_parallel₁ M eq_a eq_b) eql := by sorry
-      | Γ ._ ._ s (ν_parallel₂ M eq_a eq_b) eql := by sorry
-      | Γ ._ ._ s (ν_drop₁ M eq_a) eql := by sorry
-      | Γ ._ ._ s (ν_drop₂ M eq_a) eql := by sorry
-      | Γ ._ ._ s (@ν_swap _ M N A A' eq) eql := by sorry
-      -- begin
-      --   intros Γ s1 s2 s3 eq_12 eq_23,
-      --   cases eq_12,
-      --     case cpi.species.equiv.ξ_nil { from eq_23 },
-      --     case cpi.species.equiv.ξ_parallel : a12 a12' b12 b12' eq12_a eq12_b {
-      --       -- induction eq_23,
-      --     },
-      --     repeat { sorry }
-      -- end
-      -- | Γ nil s2 s3 eq_12 eq_23 :=
-      --   match s2, s3, eq_12, eq_23 with
-      --   | nil, nil, ξ_nil, ξ_nil := ξ_nil
-      --   end
-      -- | Γ (A1 |ₛ B1) s2 s3 eq_12 eq_23 := sorry
-      -- | Γ (ν(M1)A1) s2 s3 eq_12 eq_23 := sorry
-      -- | Γ (choice choices.nil) s2 s3 eq_12 eq_23 := sorry
-      -- | Γ (choice (choices.cons π1 A1 As1)) s2 s3 eq_12 eq_23 := sorry
-      --   intros Γ s1 s2 s3 eq_s1 eq_s2,
-      --   cases s1,
-      -- end
-      -- -- We have to unroll each case, as otherwise the totality checker times out.
-      -- | nil s2 s3 eq_12 eq_23 :=
-      --   match s2, s3, eq_12, eq_23 with
-      --   | nil, nil, ξ_nil, ξ_nil := ξ_nil
-      --   end
-      -- | (_ |ₛ _) s2 s3 eq_12 eq_23 :=
-      --   match s2, s3, eq_12, eq_23 with
-      --   | _ |ₛ _, _ |ₛ _, ξ_parallel eq_a12 eq_b12, ξ_parallel eq_a23 eq_b23 :=
-      --     ξ_parallel (trans eq_a12 eq_a23) (trans eq_b12 eq_b23)
-      --   -- | _, _, eq_12, eq_23 with
-      --   end
-      -- | (ν(_)_) s2 s3 eq_12 eq_23 :=
-      --     match s2, s3, eq_12, eq_23 with
-      --     | ν(_)_, ν(_)_, ξ_restriction M eq_12, ξ_restriction _ eq_23 :=
-      --          ξ_restriction M (trans eq_12 eq_23)
-      --     end
-      -- | (choice []) s2 s3 eq_12 eq_23 :=
-      --   match s2, s3, eq_12, eq_23 with
-      --   | choice [], choice [], ξ_choice_nil, ξ_choice_nil := ξ_choice_nil
-      --   end
-      -- | (choice ((π, A1) :: As)) s2 s3 eq_12 eq_23 :=
-      --     match s2, s3, eq_12, eq_23 with
-      --     | choice ((_, _)::_), choice ((_, _)::_), ξ_choice_cons _ eq_a12 eq_as12, ξ_choice_cons _ eq_a23 eq_as23 :=
-      --       ξ_choice_cons π (trans eq_a12 eq_a23) (trans eq_as12 eq_as23)
-      --     end
-
-
-      -- | nil nil nil ξ_nil ξ_nil := ξ_nil
-      -- | (A1 |ₛ B1) (A2 |ₛ B2) (A3 |ₛ B3) (ξ_parallel eq_a12 eq_b12) (ξ_parallel eq_a23 eq_b23) :=
-      --     ξ_parallel (trans eq_a12 eq_a23) (trans eq_b12 eq_b23)
-      -- | (ν(_)A1) (ν(_)A2) (ν(_)A3) (ξ_restriction M eq_12) (ξ_restriction _ eq_23) :=
-      --     ξ_restriction M (trans eq_12 eq_23)
-      -- | (choice []) (choice []) (choice[]) ξ_choice_nil ξ_choice_nil := ξ_choice_nil
-      -- | (choice ((_, A1)::As1)) (choice ((_, A2)::As2)) (choice ((_, A3)::As3))
-      --   (ξ_choice_cons π eq_a12 eq_as12) (ξ_choice_cons _ eq_a23 eq_as23) :=
-      --     ξ_choice_cons π (trans eq_a12 eq_a23) (trans eq_as12 eq_as23)
-      -- | _ _ _ _ _ := sorry
-
-      -- | ._ ._ ._ ξ_nil ξ_nil := ξ_nil
-      -- | ._ ._ ._ (ξ_parallel eq_a12 eq_b12) (ξ_parallel eq_a23 eq_b23) :=
-      --     ξ_parallel (trans eq_a12 eq_a23) (trans eq_b12 eq_b23)
-      -- | ._ ._ ._ (ξ_restriction M eq_12) (ξ_restriction _ eq_23) :=
-      --     ξ_restriction M (trans eq_12 eq_23)
-      -- | ._ ._ ._ ξ_choice_nil ξ_choice_nil := ξ_choice_nil
-      -- | ._ ._ ._ (ξ_choice_cons π eq_a12 eq_as12) (ξ_choice_cons _ eq_a23 eq_as23) :=
-      --     ξ_choice_cons π (trans eq_a12 eq_a23) (trans eq_as12 eq_as23)
+    end examples
   end equiv
 end species
 
