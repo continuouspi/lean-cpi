7ed6d1ca949d23060df1dd9a94d4806a82c1db92
SquidDev <git@squiddev.cc>
2020-02-25 13:23:30 +0000

    Show that any atomic species must be prime

--- src/data/cpi/species/normalise_prime.lean
+++ src/data/cpi/species/normalise_prime.lean
@@ -7,54 +7,65 @@ namespace normalise
 variables {ℍ : Type} {ω : context}
 open_locale normalise
 
-/-- Two species are "almost equal" if they are equal up to guarded choice.
-
-    We require this definition of equality in order to define primality. Guarded
-    choices are always prime, so we do not need to worry about anything inside
-    them. -/
-inductive almost_equal : ∀ {Γ : context} (A B : species ℍ ω Γ), Prop
-| refl {Γ} (A : species ℍ ω Γ) : almost_equal A A
-| parallel {Γ} {A A' B B' : species ℍ ω Γ}
-  : almost_equal A A' → almost_equal B B'
-  → almost_equal (A |ₛ B) (A' |ₛ B')
-| restriction {Γ} (M : affinity ℍ) {A A' : species ℍ ω (context.extend M.arity Γ)}
-  : almost_equal A A'
-  → almost_equal (ν(M) A) (ν(M) A')
-| choice {Γ} (As As' : choices ℍ ω Γ)
-  : almost_equal (Σ# As) (Σ# As')
-
-/-- Ensure that a list contains a single element which is 'almost_equal' to
-    another. -/
-inductive almost_equal.list {Γ : context} : list (species ℍ ω Γ) → species ℍ ω Γ → Prop
-| mk {A A' : species ℍ ω Γ} : almost_equal A A' → almost_equal.list [A] A'
-
-lemma almost_equal.list.refl {Γ : context} (A : species ℍ ω Γ) : almost_equal.list [A] A
-  := almost_equal.list.mk (almost_equal.refl A)
-
-/-- A species is n(ormalise)-prime if it normalises to itself. -/
-def prime {Γ} (A : species ℍ ω Γ) : Prop
-  := almost_equal.list (normalise_to A).fst A
-
-/-- For instance, we can show that nil is not prime -/
-example (Γ : context) : ¬ (prime (@nil ℍ ω Γ))
-| p := by { unfold prime normalise_to at p, cases p }
-
 /-- Determine if normalising a species or choice yields a list of primes. Note
     that choices are inherently prime. -/
-def yields_prime : ∀ {k} {Γ}, whole ℍ ω k Γ → Prop
-| kind.species Γ A := ∀ C ∈ (normalise_to A).fst, prime C
-| kind.choices Γ A := true
+@[reducible]
+def to_kind' : ∀ (k : kind), kind' ℍ k
+| kind.species := kind'.atom
+| kind.choices := kind'.choices
 
-/-- Show that normalisation yields a list of prime species. -/
-axiom normalise_prime :
-  ∀ {k} {Γ} (A : whole ℍ ω k Γ), yields_prime A
+axiom normalise_atom :
+  ∀ {k} {Γ} {A : whole ℍ ω k Γ}
+  , atom (to_kind' k) A → normalise A = A
 
 lemma normalise_nil {Γ} : normalise (@nil ℍ ω Γ) = nil
   := by unfold normalise normalise_to parallel.from_list
 
-/-- Show that our reduced definition of primes shows the full definition of
-    primes. -/
-axiom implies_prime : ∀ {Γ} (A : species ℍ ω Γ), prime A → species.prime A
+/-- Show that any atomic species must be prime. -/
+lemma atom_prime : ∀ {Γ} (A : species ℍ ω Γ), atom kind'.atom A → prime A
+| Γ A atomA := ⟨ λ isNil, begin
+    unfold_projs at isNil, unfold equiv at isNil,
+    rw [normalise_atom atomA] at isNil, subst isNil,
+    rw normalise_nil at atomA, cases atomA,
+  end, λ B₁ B₂ equ, begin
+    unfold_projs at equ, unfold equiv at equ,
+    rw [normalise_atom atomA] at equ, subst equ,
+
+    unfold normalise normalise_to at atomA,
+    rcases dB₁ : normalise_to B₁ with ⟨ nB₁, eqB₁, atomB₁ ⟩, rw dB₁ at atomA,
+    rcases dB₂ : normalise_to B₂ with ⟨ nB₂, eqB₂, atomB₂ ⟩, rw dB₂ at atomA,
+    unfold normalise_to._match_2 normalise_to._match_1 at atomA,
+
+    cases nB₁,
+    case list.nil {
+      -- nB₁ is nil, then B₁ ≈ nil
+      simp only [list.nil_append] at atomA,
+      suffices : parallel.from_list (normalise_to B₁).fst = normalise nil,
+        from or.inl this,
+
+      rw [dB₁, normalise_nil],
+      from rfl,
+    },
+
+    case list.cons : nB₁' nBs₁ {
+      simp only [list.cons_append] at atomA,
+      cases h : nBs₁ ++ nB₂,
+      case list.nil {
+        -- nBs₁ ++ nB₂ is nil, then B₂ is nil, thus B₁ ≈ nil
+        suffices : parallel.from_list (normalise_to B₂).fst = normalise nil,
+          from or.inr this,
+
+        simp only [dB₂, normalise_nil, (list.append_eq_nil.mp h).2],
+        from rfl,
+      },
+
+      case list.cons {
+        -- This would mean we have atom (nB₁' |ₛ parallel.from_list (nBs₁ ++ nB₂)),
+        -- which is impossible.
+        rw h at atomA, cases atomA,
+      }
+    }
+  end ⟩
 
 end normalise
 
